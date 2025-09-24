d368ca5d97e2d5a08fd551d60e4aa0bf5a83bfc6
T1Amoo <menxlee@gmail.com>
2025-09-24 11:03:30 +0800

    Initial commit: add envs module

---
+++ TAlab/talab/source/envs/agents/rsl_rl_ppo_cfg.py
@@ -0,0 +1,50 @@
+from isaaclab.utils import configclass
+from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, RslRlPpoAlgorithmCfg
+
+
+@configclass
+class BasePPORunnerCfg(RslRlOnPolicyRunnerCfg):
+    seed = 42
+    device = "cuda:0"
+    num_steps_per_env = 24
+    max_iterations = 100000
+    empirical_normalization = False
+    policy = RslRlPpoActorCriticCfg(
+        class_name="ActorCritic",
+        init_noise_std=1.0,
+        noise_std_type="scalar",
+        actor_hidden_dims=[512, 256, 128],
+        critic_hidden_dims=[512, 256, 128],
+        activation="elu",
+    )
+    algorithm = RslRlPpoAlgorithmCfg(
+        class_name="PPO",
+        value_loss_coef=1.0,
+        use_clipped_value_loss=True,
+        clip_param=0.2,
+        entropy_coef=0.005,
+        num_learning_epochs=5,
+        num_mini_batches=4,
+        learning_rate=1.0e-3,
+        schedule="adaptive",
+        gamma=0.99,
+        lam=0.95,
+        desired_kl=0.01,
+        max_grad_norm=1.0,
+        normalize_advantage_per_mini_batch=False,
+        symmetry_cfg=None,  # RslRlSymmetryCfg()
+        rnd_cfg=None,  # RslRlRndCfg()
+    )
+    clip_actions = None
+    save_interval = 100
+    experiment_name = ""
+    run_name = ""
+    logger = "tensorboard" # [tensorboard, neptune, wandb]
+    neptune_project = "talab"
+    wandb_project = "talab"
+    resume = False
+    load_run = ".*"
+    load_checkpoint = "model_.*.pt"
+
+    def __post_init__(self):
+        pass
